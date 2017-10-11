9cb81cdf64b1fc8a297d0707f8cebacbe6b7751c
Brian Phillips <brian.phillips@mindbodyonline.com>
2017-10-10 17:40:19 -0700

    creating basic layout (no components)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -23,15 +22,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>
+          <h1>Readable</h1>
+        </header>
+
+        <aside className="categories">
+          <ul>
+            <li><a href="#">React</a></li>
+            <li><a href="#">Redux</a></li>
+            <li><a href="#">Udacity</a></li>
+          </ul>
+        </aside>
+        
+        <section className="posts">
+          <ul className="post-list">
+            <li className="post">
+              <span className="post-score">5</span>
+              <h4><a href="#">React</a></h4>
+              <h3><a href="#">5 Reasons Why React is the Best JS Framework</a></h3>
+              <div className="post-meta">
+                <span className="post-author">Brian Phillips</span> · <span className="post-comment-count">5 Comments</span>
+              </div>
+            </li>
+          </ul>
+        </section>
+
         <p>
-          Talking to the backend yields these categories: <br/>
+          Categories: <br/>
           {this.state.backend}
         </p>
       </div>
