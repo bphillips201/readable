358e5518e03ef1ff068f1ddeb98ba694e9db1d6d
Brian Phillips <brian.phillips@mindbodyonline.com>
2017-10-17 11:06:03 -0700

    added basic styling for category list

--- src/components/category_list.js
+++ src/components/category_list.js
@@ -1,3 +1,4 @@
+import '../styles/categories.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ReadableAPI from '../utils/ReadableAPI';
@@ -16,11 +17,14 @@ class CategoryList extends React.Component {
   
   render() {
     return(
-      <ul className="category-list">
-        {this.state.categories.map((cat) => (
-          <li key={cat.name}><Link to={cat.path}>{capitalize(cat.name)}</Link></li>
-        ))}
-      </ul>
+      <div>
+        <h3>Categories</h3>
+        <ul className="category-list">
+          {this.state.categories.map((cat) => (
+            <li key={cat.name}><Link to={`/${cat.path}`}>{capitalize(cat.name)}</Link></li>
+          ))}
+        </ul>
+      </div>
     )
   }
 }
