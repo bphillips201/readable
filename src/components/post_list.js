f533ce908dfff8e9261388ac5313c3b73fe5843d
Brian Phillips <brian.phillips@mindbodyonline.com>
2017-10-20 11:11:59 -0700

    added post sorting by vote score

--- src/components/post_list.js
+++ src/components/post_list.js
@@ -6,16 +6,34 @@ import { connect } from 'react-redux';
 
 class PostList extends React.Component {
 
+  state = {
+    sortDir: "DESC"
+  }
+
+  toggleSort = (dir) => {
+    dir === "DESC" ? this.setState({ sortDir: "ASC" }) : this.setState({ sortDir: "DESC" });
+  }
+
+  sort = (dir, a, b) => {
+    return dir === "DESC" ? a < b : a > b;
+  } 
+
   render() {
+    const { sortDir } = this.state;
+
     return(
-      <ul className="post-list">
-        {this.props.posts
-          .sort((a, b) => a.voteScore < b.voteScore)
-          .filter((post) => post.deleted !== true)
-          .map((post) => (
-            <Post post={post} key={post.id}/>
-        ))}
-      </ul>
+      <div>
+        <p className="align-right">Sort by Votes <button onClick={() => this.toggleSort(sortDir)} className="btn-link">{sortDir}</button></p>
+
+        <ul className="post-list">
+          {this.props.posts
+            .sort((a, b) => this.sort(sortDir, a.voteScore, b.voteScore))
+            .filter((post) => post.deleted !== true)
+            .map((post) => (
+              <Post post={post} key={post.id}/>
+          ))}
+        </ul>
+      </div>
     )
   }
 }
